aee5896237aafa41dd52e02020c0582384d8bf6a
Brad Palagi <brad.palagi@grianger.com>
2025-08-10 19:40:43 -0500

    added autosave

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -27,7 +27,7 @@ return {
             enabled = true,
             clear_in_insert_mode = false,
             download_remote_images = true,
-            only_render_image_at_cursor = false,
+            only_render_image_at_cursor = true,
           },
         },
         window_overlap_clear_enabled = true,
